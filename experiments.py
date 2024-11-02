b630e8a4e56d14414b0eda5190ea147791d95b05
Rineez Ahmed <rineez@gmail.com>
2024-11-02 23:57:30 +0530

    Use Dijkstra to compare result accuracy

--- experiments.py
+++ experiments.py
@@ -1,9 +1,8 @@
 from tsp_problems import run_experiment
 
 
-
 if __name__ == "__main__":
-    for nodes in range(5,8): # 5 to 7 nodes
+    for nodes in range(5,7): # 5 to 7 nodes
         for maxiter in range(1, 3): # maxiter=1 and maxiter=2
             run_experiment(nodes, optimizer_choice='cobyla', optimizer_maxiter=maxiter, use_simulator=True, save_graph=True)
             run_experiment(nodes, optimizer_choice='spsa', optimizer_maxiter=maxiter, use_simulator=True, save_graph=True)
--- tsp_problems.py
+++ tsp_problems.py
@@ -69,9 +69,9 @@ def run_experiment(
         use_simulator=True,
         save_graph=True):
     print("\n============================================\n")
-    print(f"Running Experiment with: Cities={num_of_nodes}, Optimizer={optimizer_choice}, Maxiter={optimizer_maxiter}")
+    print(f"Running Experiment in {'Simulator' if use_simulator else 'IBM Cloud'} with:")
+    print(f"Cities={num_of_nodes}, Optimizer={optimizer_choice}, Maxiter={optimizer_maxiter}")
 
-    start_time = time.time()
     # Create problem definition
     match num_of_nodes:
         case 3: distances, cities = create_tsp_graph_3nodes()
@@ -81,28 +81,52 @@ def run_experiment(
 
     print(f"Solving TSP for {len(cities)} cities:\n", cities, "\nDistance Matrix:\n", distances)
 
-    optimalPath, err = solve_tsp_with_qaoa(distances, cities,
+    # print("\nUsing Dijkstra's algorith..")
+    # start_time = time.time()
+    # best_path, shortest_distance = solve_tsp_with_dijkstra(distances, cities)
+
+    # end_time = time.time()
+    # runtime_duration = end_time - start_time
+    # minutes = runtime_duration // 60
+    # seconds = runtime_duration % 60
+    # print(f"Dijkstra completed in: {minutes:.0f} minutes {seconds:.2f} seconds")
+
+    # if best_path and save_graph:
+    #     # Generate solution graph
+    #     print("\nVisualizing Optimal Path computed by Dijkstra's:")
+    #     try:
+    #         visualize_graph(distances, cities, best_path, save=True, save_prefix='dijkstra_')
+    #     except Exception as ex:
+    #         print(f"Error in visualize_graph: {ex}")
+    #         traceback.print_exc()
+
+
+    print("\nUsing QAOA..")
+    start_time = time.time()
+
+    optimal_path, err = solve_tsp_with_qaoa(distances, cities,
         optimizer_choice=optimizer_choice,
         optimizer_maxiter=optimizer_maxiter,
         use_simulator=use_simulator
         )
-    run_mode = 'SIM' if use_simulator else 'QPU'
-    save_prefix = f'{run_mode}_{optimizer_choice}-{optimizer_maxiter}_'
-    
-    if not err and save_graph:
-        # Generate solution graph
-        print("\nVisualizing Optimal Path:")
-        try:
-            visualize_graph(distances, cities, optimalPath, save=True, save_prefix=save_prefix)
-        except Exception as ex:
-            print(f"Error in visualize_graph: {ex}")
-            traceback.print_exc()
 
     end_time = time.time()
     runtime_duration = end_time - start_time
     minutes = runtime_duration // 60
     seconds = runtime_duration % 60
-    print(f"Script ran for: {minutes:.0f} minutes {seconds:.2f} seconds")
+    print(f"QAOA completed in: {minutes:.0f} minutes {seconds:.2f} seconds")
+
+    run_mode = 'SIM' if use_simulator else 'QPU'
+    save_prefix = f'{run_mode}_{optimizer_choice}-{optimizer_maxiter}_'
+
+    if not err and save_graph:
+        # Generate solution graph
+        print("\nVisualizing Optimal Path:")
+        try:
+            visualize_graph(distances, cities, optimal_path, save=True, save_prefix='qaoa_'+save_prefix)
+        except Exception as ex:
+            print(f"Error in visualize_graph: {ex}")
+            traceback.print_exc()
 
 
 if __name__ == "__main__":
