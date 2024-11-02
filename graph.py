51197ebcfa11796c316f42934b8e2ac0f776d171
Rineez Ahmed <rineez@gmail.com>
2024-11-02 23:56:38 +0530

    update save_prefix in graph

--- graph.py
+++ graph.py
@@ -79,7 +79,7 @@ def visualize_graph(distances, cities, path=None, save=False,save_prefix=''):
     plt.tight_layout()
     if save:
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        savefig_name = f"{save_prefix}qaoa_{len(cities)}_nodes_{timestamp}.png"
+        savefig_name = f"{save_prefix}{len(cities)}_nodes_{timestamp}.png"
         savefig_name = os.path.join("graphs", savefig_name)
         plt.savefig(savefig_name, format="png")
     else:
