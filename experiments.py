7b5ba0915b06a0ed856c00e1f4428c4b8eabeb78
Rineez Ahmed <rineez@gmail.com>
2024-11-06 01:22:52 +0530

    Add TODO Experiments

--- experiments.py
+++ experiments.py
@@ -1,8 +1,22 @@
 from tsp_problems import run_experiment
-
+from tsp_problems import Algo
 
 if __name__ == "__main__":
-    for nodes in range(5,7): # 5 to 7 nodes
-        for maxiter in range(1, 3): # maxiter=1 and maxiter=2
-            run_experiment(nodes, optimizer_choice='COBYLA', optimizer_maxiter=maxiter, use_simulator=True, save_graph=True)
-            run_experiment(nodes, optimizer_choice='SPSA', optimizer_maxiter=maxiter, use_simulator=True, save_graph=True)
+    # IN PROGRESS BY RINEEZ
+    # run_experiment(4, optimizer_choice='COBYLA', optimizer_maxiter=3, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 15s
+
+    # run_experiment(5, optimizer_choice='SPSA', optimizer_maxiter=1, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 2m 45s
+    # run_experiment(5, optimizer_choice='COBYLA', optimizer_maxiter=1, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 2m 50s
+    
+    # run_experiment(5, optimizer_choice='SPSA', optimizer_maxiter=2, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 4m 35s
+    # run_experiment(5, optimizer_choice='COBYLA', optimizer_maxiter=2, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 5m
+
+    # run_experiment(5, optimizer_choice='SPSA', optimizer_maxiter=3, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 7m 45s
+    # run_experiment(5, optimizer_choice='COBYLA', optimizer_maxiter=3, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 8m
+
+    # NEXT TO RUN
+    run_experiment(6, optimizer_choice='SPSA', optimizer_maxiter=1, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 2m 30s
+    run_experiment(6, optimizer_choice='COBYLA', optimizer_maxiter=1, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 2m 40s
+    
+    run_experiment(6, optimizer_choice='SPSA', optimizer_maxiter=2, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 4m 30s
+    run_experiment(6, optimizer_choice='COBYLA', optimizer_maxiter=2, use_simulator=False, save_graph=True, algorithms=Algo.QAOA) # 4m 45s
